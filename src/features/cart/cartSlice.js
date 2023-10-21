2ebdcd2762973f4f29a0cc44ef479c5c36f2c424
ItryCode <sonuhrajeev@gmail.com>
2023-10-21 13:51:08 +0530

    Added more reducers

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -33,9 +33,30 @@ const cartSlice = createSlice({
       toast.success("Item Added To Cart");
     },
 
-    clearCart: (state) => {},
-    removeItem: (state, action) => {},
-    editItem: (state, action) => {},
+    clearCart: (state) => {
+      localStorage.setItem("cart", JSON.stringify(defaultState));
+      return defaultState;
+    },
+    removeItem: (state, action) => {
+      const { cartID } = action.payload;
+      const product = state.cartItems.find((item) => item.cartID === cartID);
+      state.cartItems = state.cartItems.filter(
+        (item) => item.cartID === cartID
+      );
+      state.numItemsInCart -= product.amount;
+      state.cartTotal -= product.price * product.amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.error("Item Removed From Cart");
+    },
+    editItem: (state, action) => {
+      const { cartID, amount } = action.payload;
+      const item = state.cartItems.find((item) => item.cartID === cartID);
+      state.numItemsInCart += amount - item.amount;
+      state.cartTotal += item.price * (amount - item.amount);
+      item.amount = amount;
+      cartSlice.caseReducers.calculateTotals(state);
+      toast.success("Cart updated");
+    },
     calculateTotals: (state) => {
       state.tax = 0.1 * state.cartTotal;
       state.orderTotal = state.cartTotal + state.shipping + state.tax;
