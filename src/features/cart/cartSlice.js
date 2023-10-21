1afdbd5a8a81d4d0be3db2033c368e71a2e4a56a
ItryCode <sonuhrajeev@gmail.com>
2023-10-21 13:20:56 +0530

    Refactored Code in cartSlice.js

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -28,15 +28,19 @@ const cartSlice = createSlice({
       }
       state.numItemsInCart += product.amount;
       state.cartTotal += product.price * product.amount;
-      state.tax = 0.1 * state.cartTotal;
-      state.orderTotal = state.cartTotal + state.shipping + state.tax;
-      localStorage.setItem("cart", JSON.stringify(state));
+
+      cartSlice.caseReducers.calculateTotals(state);
       toast.success("Item Added To Cart");
     },
 
     clearCart: (state) => {},
     removeItem: (state, action) => {},
     editItem: (state, action) => {},
+    calculateTotals: (state) => {
+      state.tax = 0.1 * state.cartTotal;
+      state.orderTotal = state.cartTotal + state.shipping + state.tax;
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
   },
 });
 
